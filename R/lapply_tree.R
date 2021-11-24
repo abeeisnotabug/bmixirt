23f69c982d8b92c85f04350d1b51da5e1ea202c8
Maximilian Bee <richard.maximilian.bee@fu-jena.de>
2021-11-24 10:55:45 +0100

    Add a function that makes lapply trees.

---
+++ R/lapply_tree.R
@@ -0,0 +1,32 @@
+make_lapply_tree_expr <- function(iterators, iterator_names, body) {
+  if (length(iterators) > 1) {
+    sprintf(
+      "lapply(%s, function(%s) {%s})",
+      iterators[1],
+      iterator_names[1],
+      make_lapply_tree_expr(iterators[-1], iterator_names[-1], body)
+    )
+  } else {
+    sprintf(
+      "lapply(%s, function(%s) %s)",
+      iterators[1],
+      iterator_names[1],
+      body
+    )
+  }
+}
+
+lapply_tree <- function(iterators, iterator_names, my_expr) {
+  if (!(is.character(iterators) & is.character(iterator_names))) {
+    stop("iterators and iterator_names must be character vectors.")
+  } else if (length(iterators) != length(iterator_names)) {
+    stop("iterators and iterator_names must have same length.")
+  } else {
+    eval(
+      str2expression(
+        make_lapply_tree_expr(iterators, iterator_names, body = deparse1(substitute(my_expr, env = environment()), collapse = ""))
+      ),
+      envir = parent.frame(n = 2)
+    )
+  }
+}
