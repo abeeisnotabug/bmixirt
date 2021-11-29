4f6ac616e56887deea90b51e5c25492acece01a5
Maximilian Bee <richard.maximilian.bee@fu-jena.de>
2021-11-29 13:19:41 +0100

    Update lapply_tree to only take one named argument for the iterators.

--- R/lapply_tree.R
+++ R/lapply_tree.R
@@ -1,30 +1,28 @@
-make_lapply_tree_expr <- function(iterators, iterator_names, body) {
+make_lapply_tree_expr <- function(iterators, body) {
   if (length(iterators) > 1) {
     sprintf(
       "lapply(%s, function(%s) {%s})",
       iterators[1],
-      iterator_names[1],
-      make_lapply_tree_expr(iterators[-1], iterator_names[-1], body)
+      names(iterators[1]),
+      make_lapply_tree_expr(iterators[-1], body)
     )
   } else {
     sprintf(
       "lapply(%s, function(%s) %s)",
       iterators[1],
-      iterator_names[1],
+      names(iterators[1]),
       body
     )
   }
 }
 
-lapply_tree <- function(iterators, iterator_names, my_expr) {
-  if (!(is.character(iterators) & is.character(iterator_names))) {
-    stop("iterators and iterator_names must be character vectors.")
-  } else if (length(iterators) != length(iterator_names)) {
-    stop("iterators and iterator_names must have same length.")
+lapply_tree <- function(iterators, my_expr) {
+  if (!is.character(iterators) || is.null(names(iterators))) {
+    stop("iterators must be a named character vector.")
   } else {
     eval(
       str2expression(
-        make_lapply_tree_expr(iterators, iterator_names, body = paste(deparse(substitute(my_expr, env = environment())), collapse = "\n"))
+        make_lapply_tree_expr(iterators, body = paste(deparse(substitute(my_expr, env = environment())), collapse = "\n"))
       ),
       envir = parent.frame(n = 2)
     )
